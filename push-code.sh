b0d527b540c81983175f524fe362a8de3d1d30aa
dzsshao <dzsshao@gmail.com>
2021-06-17 18:19:10 +0200

    edit

---
+++ push-code.sh
@@ -0,0 +1,10 @@
+git config --global credential.helper store
+
+git config --global user.email "dzsshao@gmail.com"
+git config --global user.name "dzsshao"
+
+git add .
+git commit -m "edit"
+
+git pull
+git push
